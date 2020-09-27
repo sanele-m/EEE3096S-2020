40f1f1d871c499bff5473e4e5243d54b86b05314
sanele-m <mdlsah001@myuct.ac.za>
2020-09-27 14:51:53 +0100

    switch added to toggle led

--- blink.py
+++ blink.py
@@ -1,12 +1,19 @@
 import RPi.GPIO as GPIO
 import time 
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(11,GPIO.OUT)
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setup(11,GPIO.OUT) #set pin  11 as out
+GPIO.setup(13,GPIO.IN, pull_up_down=GPIO.PUD_UP) #set pin 13 as input
 
-for i in range(4):
-	GPIO.output(11,1)
-	time.sleep(0.5)
-	GPIO.output(11,0)
-	time.sleep(0.5)
-GPIO.cleanup()
+def Button (channel):  #this function will be called when the button is pressed
+	GPIO.output(11,1) #Turn ON the LED 
+
+GPIO.add_event_detect(13, GPIO.FALLING,callback= Button,bouncetime=200) #interupt that will call the button function
+
+try:
+	while 1:
+		if (GPIO.input(13)==1): # this check if the switch is is not pressed 
+			GPIO.output(11,0) # and if the switch is not pressed this part switches Led OFF
+except KeyboardInterrupt:
+	GPIO.remove_event_detect(13)
+	GPIO.cleanup()
